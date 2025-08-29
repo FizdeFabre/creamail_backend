e7e97f182697435bb10ec34e550a51f610289ccc
FizdeFabre <fachocapic@gmail.com>
2025-08-29 11:03:18 +0200

    Correction problème Destinataire edit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { processOnce } from "./cronJob.js";
+import { processOnce } from "./cron.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
